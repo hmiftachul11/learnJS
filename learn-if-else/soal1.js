585d2db0516f88e7fa117e0ab34ea2e697817b02
hmiftachul11 <hmiftachul11@gmail.com>
2023-12-09 03:50:17 +0700

    Learn If Else

---
+++ learn-if-else/soal1.js
@@ -0,0 +1,40 @@
+function cekBilangan() {
+  let tryAgain = true;
+
+  while (tryAgain) {
+    let check = prompt("Masukkan Angka:");
+
+    // Mengonversi input ke tipe data number
+    const checkData = parseFloat(check);
+
+    if (!isNaN(checkData)) {
+      // Memeriksa apakah input adalah angka
+      let message = "";
+
+      if (checkData >= 0) {
+        if (checkData === 0) {
+          message = "bilangan 0";
+        } else if (checkData % 2 === 0) {
+          message = "bilangan genap";
+        } else {
+          message = "ganjil";
+        }
+      } else {
+        message = "bilangan negatif";
+      }
+
+      // Menampilkan hasil dengan alert
+      alert(`Hasil: ${message}`);
+
+      // Menanyakan apakah pengguna ingin mencoba lagi
+      tryAgain = confirm("Apakah Anda ingin mencoba lagi?");
+    } else {
+      alert("Input bukan angka. Silakan coba lagi.");
+    }
+  }
+
+  alert("Terima kasih! Program berhenti.");
+}
+
+// Memanggil fungsi untuk pertama kali
+cekBilangan();
